1ef2d00a23530baaa9a7ba71be17ab9d752b070e
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2019-10-02 00:09:29 -0700

    New branch, not working

--- add/add.py
+++ add/add.py
@@ -6,3 +6,11 @@ def add(*lists):
             l+=[x+y]
         result.append(l)
     return result
+
+"""
+matrix1 = [[1, -2], [-3, 4]]
+matrix2 = [[2, -1], [0, -1]]
+matrix3 = [[1, -2, 3], [-4, 5, -6], [7, -8, 9]]
+matrix4 = [[1, 1, 0], [1, -2, 3], [-2, 2, -2]]
+matrix5 = [[1, 0], [2, 1]]
+"""
