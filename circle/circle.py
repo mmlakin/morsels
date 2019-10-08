98f9af7a70c37d44c3d45266a6ef4672de6b650e
Matt Lakin <mmlakin@gmail.com>
2019-10-07 18:43:47 -0700

    Add circle test with solutions

---
+++ circle/circle.py
@@ -0,0 +1,32 @@
+from math import pi
+
+class Circle:
+    def __init__(self, radius = 1):
+        self._radius = radius
+
+    def __repr__(self):
+        return "Circle({})".format(self._radius)
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        if radius > 0:
+            self._radius = radius
+        else:
+            raise ValueError("Radius cannot be negative")
+
+    @property
+    def diameter(self):
+        return (self._radius * 2)
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self._radius = (diameter / 2)
+        self._diameter = diameter
+
+    @property
+    def area(self):
+        return (pi * self._radius**2)
