0aa31f0346bc9c0b07e6e99a50d35bf7907b65af
mmlakin <49107138+mmlakin@users.noreply.github.com>
2019-10-18 01:26:05 -0700

    fix_csv both bonuses (#5)
    * Working base exercise, no bonus
    
    * Completed both bonuses, final

---
+++ fix_csv/fix_csv.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env/python3
+
+import sys, csv, argparse
+
+def replace_file(infile, outfile, delim, quote):
+    with open(infile, mode="rt", encoding="utf-8") as ifile:
+        with open(outfile, mode="wt", encoding="utf-8") as ofile:
+            if delim == None or quote == None:
+                sniffed = csv.Sniffer().sniff(ifile.read())
+                if delim == None: delim = sniffed.delimiter
+                if quote == None: quote = sniffed.quotechar
+                ifile.seek(0)
+
+            csv.register_dialect('dynamic',
+                                 delimiter=delim,
+                                 quotechar=quote)
+            csvwriter = csv.writer(ofile)
+            for row in csv.reader(ifile, dialect='dynamic'):
+                csvwriter.writerow(row)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    parser.add_argument('--in-delimiter', dest='delim', type=str,
+                        help='Input file delimiter', metavar='delimiter')
+    parser.add_argument('--in-quote', dest='quote', type=str,
+                        help='Input file comma', metavar='quote')
+
+    return parser.parse_args()
+
+def main():
+
+    args = parse_arguments()
+    replace_file(args.infile, args.outfile, args.delim, args.quote)
+
+if __name__ == "__main__":
+    main()
