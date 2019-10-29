d48ac6b3f9b1e0104c52a74960e95b8184c38294
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2019-10-28 22:49:36 -0700

    Update count_words final bonus regex
    Use regex to find words with apostrophes for final bonus

--- count_words/count.py
+++ count_words/count.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-
-"""
-count.py - Take a string and return a dict of each word in the string and the number of times it appears in the string.
-"""
+"""count.py - Take a string and return a dict of each word in the string and the number of times it appears in the string"""
 
 from string import punctuation as punctuationmarks
+import re
 
 def count_words(line: str) -> dict:
-    """
-
-    """
-
-    linewords = line.lower().split(' ')
-
+    """ Use re.findall to return a list of words with apostrophes included """
+    regex=r"[\w']+"
+    linewords = re.findall(regex, line.lower())
     return {
-        word.strip(punctuationmarks):linewords.count(word)
+        word:linewords.count(word)
         for word in set(linewords)
     }
