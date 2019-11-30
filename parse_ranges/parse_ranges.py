fa1f30b29bf2e3e4bc0daee1a9197e384a586385
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2019-11-30 03:06:30 -0800

    Tune parse_ranges for pylint

--- parse_ranges/parse_ranges.py
+++ parse_ranges/parse_ranges.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 
+"""
+parse_ranges.py - Take string of ints and ranges and return iterator
+"""
 import re
 
-re_split = r',\s*'
-re_single = r'^\d+$'
-re_range = r'^(\d+)\-(\d+)$'
-re_arrow = r'^(\d+)\-\>.*$'
+RE_SPLIT = r',\s*'
+RE_SINGLE = r'^\d+$'
+RE_RANGE = r'^(\d+)\-(\d+)$'
+RE_ARROW = r'^(\d+)\-\>.*$'
 
 def parse_ranges(str_) -> iter:
     """  Takes a string of numbers and ranges, like:
         "1,2-2,3-6,8,11-13"
         and yields each integer
     """
-    items = re.split(',\s*', str_)
+    items = re.split(RE_SPLIT, str_)
 
     for item in items:
-        singlelist = re.findall(re_single, item)
-        rangelist = re.findall(re_range, item)
-        arrowlist = re.findall(re_arrow, item)
+        singlelist = re.findall(RE_SINGLE, item)
+        rangelist = re.findall(RE_RANGE, item)
+        arrowlist = re.findall(RE_ARROW, item)
 
         for digit in singlelist + arrowlist:
             digit = int(digit)
             yield digit
 
-        for start,stop in rangelist:
-            start,stop = int(start), int(stop)
+        for start, stop in rangelist:
+            start, stop = int(start), int(stop)
             stop += 1
-            yield from range(start,stop)
-
+            yield from range(start, stop)
