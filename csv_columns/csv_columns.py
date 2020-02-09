71b342c415594cf9a3026cf73cfc81525a073ab7
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-02-08 18:19:01 -0800

    Update csv_columns using itertools & setdefault
    Instead of filling out values with a loop, use zip_longest with
    fillvalue from the itertools module.  Also use dict.setdefault() to
    create the initial list value for each key.  setdefault returns the
    value of the key, which in this case is the list itself, which is why
    you can use .append directly on it.

--- csv_columns/csv_columns.py
+++ csv_columns/csv_columns.py
@@ -5,8 +5,8 @@ csv_columns.py
     column data for each header.
 """
 import csv
-import sys
 import collections
+import itertools
 def csv_columns(input_file_obj, *, headers=None, missing=None) -> dict:
     """ Takes a file and returns a dict mapping headers to data"""
     csv_file = csv.reader(input_file_obj)
@@ -16,17 +16,6 @@ def csv_columns(input_file_obj, *, headers=None, missing=None) -> dict:
     else:
         csv_headers = next(csv_file)
     for row in csv_file:
-        for _ in range(len(csv_headers) - len(row)):
-            row.append(missing)
-        for header, value in zip(csv_headers, row):
-            if header not in csv_dict:
-                csv_dict[header] = []
-            csv_dict[header].append(value)
+        for header, value in itertools.zip_longest(csv_headers, row, fillvalue=missing):
+            csv_dict.setdefault(header, []).append(value)
     return csv_dict
-
-def main(args):
-    """ Main function """
-    csv_columns(args)
-
-if __name__ == "__main__":
-    main(sys.argv[1])
