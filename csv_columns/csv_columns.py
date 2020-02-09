87d40556992b94aa8e9b7e98314096eaa826c4c7
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-02-08 18:32:39 -0800

    Update csv_columns, tuple unpack & comprehension
    Instead of creating a dict filled with lists, then updating each list
    with append(), use tuple unpacking to zip() headers and all rows at
    once, setting the header key to a tuple containing all the values.
    
    Also, instead of creating a dict, return a dictionary comprehension
    directly.  Instead of creating a list of rows from the csv reader
    object, just iterate over the csv reader object itself in the dictionary
    comprehension.

--- csv_columns/csv_columns.py
+++ csv_columns/csv_columns.py
@@ -5,17 +5,15 @@ csv_columns.py
     column data for each header.
 """
 import csv
-import collections
 import itertools
 def csv_columns(input_file_obj, *, headers=None, missing=None) -> dict:
     """ Takes a file and returns a dict mapping headers to data"""
     csv_file = csv.reader(input_file_obj)
-    csv_dict = collections.OrderedDict()
     if headers:
         csv_headers = headers
     else:
         csv_headers = next(csv_file)
-    for row in csv_file:
-        for header, value in itertools.zip_longest(csv_headers, row, fillvalue=missing):
-            csv_dict.setdefault(header, []).append(value)
-    return csv_dict
+    return {
+        header: value
+        for header, *value in itertools.zip_longest(csv_headers, *csv_file, fillvalue=missing)
+    }
