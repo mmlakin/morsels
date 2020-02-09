6fea4ea666d8056f30614435a00a0308285f969a
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-02-08 19:48:45 -0800

    Update csv_columns with function docstring

--- csv_columns/csv_columns.py
+++ csv_columns/csv_columns.py
@@ -8,6 +8,7 @@ import csv
 from collections import defaultdict
 
 def csv_columns(input_file_obj, *, headers=None, missing=None) -> dict:
+    """Takes a file object and returns a dict mapping headers to values"""
     csv_file = csv.DictReader(input_file_obj, fieldnames=headers, restval=missing)
     csv_dict = defaultdict(list)
     for row in csv_file:
