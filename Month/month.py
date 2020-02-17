2c658ed96c8b4f0202d37169984e456c15d734c6
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-02-16 17:42:14 -0800

    Month implement dataclass

--- Month/month.py
+++ Month/month.py
@@ -7,24 +7,30 @@ A Month class to represent a specific month and year.
 Month objects have two different string representations, and are comparable to each other using equality and ordering operations.  Month objects are not comparable to other types.
 
 Bonus 1: Implement first_day and last_day attributes.
+Bonus 2: Implement from_date() factory function and strftime() instance method.
+Bonus 3: Make Month objects memory efficient, immutable, and hashable
 
 """
 
 import datetime
 import calendar
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True, order=True)
 class Month:
     """Month class Month(year, month) comparable & orderable to self"""
+
+    year: int
+    month: int
 
     __slots__ = ["year", "month"]
 
     def from_date(from_date_object):
         return Month(from_date_object.year, from_date_object.month)
 
-    def __init__(self, year, month):
-        object.__setattr__(self, "year", year)
-        object.__setattr__(self, "month", month)
+    def strftime(self, strftime_format_string):
+        return self.first_day.strftime(strftime_format_string)
 
     @property
     def first_day(self):
@@ -35,49 +41,5 @@ class Month:
         _, days_in_month = calendar.monthrange(self.year, self.month)
         return datetime.date(self.year, self.month, days_in_month)
 
-    def strftime(self, strftime_format_string):
-        return self.first_day.strftime(strftime_format_string)
-
-    def __repr__(self):
-        return f"Month({self.year}, {self.month})"
-
     def __str__(self):
         return f"{self.year}-{self.month:02}"
-
-    def __setattr__(self, attr, value):
-        raise AttributeError
-
-    __delattr__ = __setattr__
-
-    def __hash__(self):
-        return hash((self.year, self.month))
-
-    def __eq__(self, other):
-        if isinstance(other, Month):
-            return (self.year, self.month) == (other.year, other.month)
-        else:
-            return NotImplemented
-
-    def __lt__(self, other):
-        if isinstance(other, Month):
-            return (self.year, self.month) < (other.year, other.month)
-        else:
-            return NotImplemented
-
-    def __gt__(self, other):
-        if isinstance(other, Month):
-            return (self.year, self.month) > (other.year, other.month)
-        else:
-            return NotImplemented
-
-    def __le__(self, other):
-        if isinstance(other, Month):
-            return (self.year, self.month) <= (other.year, other.month)
-        else:
-            return NotImplemented
-
-    def __gt__(self, other):
-        if isinstance(other, Month):
-            return (self.year, self.month) >= (other.year, other.month)
-        else:
-            return NotImplemented
