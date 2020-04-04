dd92c0f39a21aba5506538527e82e6ebd7dcc790
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-04-04 16:52:10 -0700

    Add reformat_diary base with RE

---
+++ reformat_diary/reformat_diary.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+  reformat_diary.py - Turn a text file with many diary entries into many text
+  files, one for each entry
+"""
+
+import re
+
+
+RE_DATE = re.compile(r"^\d{4}[-/]\d{2}[-/]\d{2}")
+
+
+def entries_by_date(diary_file) -> list:
+    cur_date, cur_entry = None, ""
+    for diary_line in diary_file:
+        if RE_DATE.search(diary_line):
+            if cur_date:
+                yield cur_date, cur_entry.strip()
+            cur_date, cur_entry = diary_line.rstrip(), ""
+        else:
+            cur_entry += diary_line
+    yield cur_date, cur_entry.strip()
