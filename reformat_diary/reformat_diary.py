9761a633678a941621d22b55ca995923a681dda8
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-04-26 15:42:56 -0700

    reformat_diary bonus 1

--- reformat_diary/reformat_diary.py
+++ reformat_diary/reformat_diary.py
@@ -11,13 +11,20 @@ import re
 RE_DATE = re.compile(r"^\d{4}[-/]\d{2}[-/]\d{2}")
 
 
+def clean_entry(entry):
+    entry = entry.replace("&nbsp;", " ")
+    entry = entry.replace("&quot;", '"')
+    entry = entry.replace("&amp;", "&")
+    return entry.strip()
+
+
 def entries_by_date(diary_file) -> list:
     cur_date, cur_entry = None, ""
     for diary_line in diary_file:
         if RE_DATE.search(diary_line):
             if cur_date:
-                yield cur_date, cur_entry.strip()
+                yield cur_date, clean_entry(cur_entry)
             cur_date, cur_entry = diary_line.rstrip(), ""
         else:
             cur_entry += diary_line
-    yield cur_date, cur_entry.strip()
+    yield cur_date, clean_entry(cur_entry)
