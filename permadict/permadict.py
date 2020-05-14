daa967920c1400d2b9cecf64723c647ae63eeec7
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-05-14 15:33:09 -0700

    Permadict all bonuses

--- permadict/permadict.py
+++ permadict/permadict.py
@@ -15,14 +15,85 @@
 """
 
 
-class PermaDict(dict):
+class PermaDict:
+    VERBOSE = False
+
+    def __init__(self, init=None, *, silent=False, **kwargs):
+        if self.VERBOSE:
+            print(f"init_kwargs:{kwargs}")
+        self._dict = dict()
+        self._silent = silent
+        if init is not None:
+            self.update(init)
+        if kwargs is not None:
+            self.update(kwargs)
+
     def __setitem__(self, key, value):
-        print(f"key: {key} - value: {value}")
-        key = str(key)
         if key in self.keys():
-            raise KeyError("Unable to set values in PermaDict.")
-        else:
-            setattr(self, key, value)
+            if self._silent is False:
+                raise KeyError("Unable to set values in PermaDict.")
+            return
+        if self.VERBOSE is True:
+            print(f"Setting {key} to {value}")
+        self._dict[key] = value
+
+    def __delitem__(self, item):
+        del self._dict[item]
+
+    def __getitem__(self, item):
+        return self._dict[item]
 
     def __eq__(self, other):
-        return self.items() == other
+        return self._dict == other
+
+    def __repr__(self):
+        return str(self._dict)
+
+    def __iter__(self):
+        yield from self._dict.keys()
+
+    def keys(self):
+        return self._dict.keys()
+
+    def values(self):
+        return self._dict.values()
+
+    def items(self):
+        return self._dict.items()
+
+    def pop(self, key):
+        return self._dict.pop(key)
+
+    def force_set(self, key, value):
+        self._dict[key] = value
+
+    def update(self, update_iter=None, *update_dicts, force=False, **update_kwargs):
+        if self.VERBOSE:
+            print(f"update_iter: {update_iter}")
+            print(f"update_dicts: {update_dicts}")
+        if update_iter:
+            try:
+                for k, v in update_iter:
+                    if force is True:
+                        self.force_set(k, v)
+                    else:
+                        self[k] = v
+            except (ValueError, TypeError):
+                for k in update_iter:
+                    if force is True:
+                        self.force_set(k, update_iter[k])
+                    else:
+                        self[k] = update_iter[k]
+        if update_dicts:
+            for update_dict in update_dicts:
+                if force is True:
+                    self.force_set(k, update_dict[k])
+                else:
+                    self[k] = update_dict[k]
+
+        if update_kwargs:
+            for k in update_kwargs:
+                if force is True:
+                    self.force_set(k, update_kwargs[k])
+                else:
+                    self[k] = update_kwargs[k]
--- permadict/test_permadict.py
+++ permadict/test_permadict.py
@@ -24,14 +24,14 @@ class PermaDictTests(unittest.TestCase):
         self.assertEqual(PermaDict({1: 2, 3: 4}), {1: 2, 3: 4})
 
     def test_can_iterate(self):
-        d = PermaDict({'a': 'b', 'c': 'd'})
-        self.assertEqual(set(d), {'a', 'c'})
+        d = PermaDict({"a": "b", "c": "d"})
+        self.assertEqual(set(d), {"a", "c"})
 
     def test_has_keys_values_and_items(self):
-        d = PermaDict({'a': 'b', 'c': 'd'})
-        self.assertEqual(set(d.keys()), {'a', 'c'})
-        self.assertEqual(set(d.values()), {'b', 'd'})
-        self.assertEqual(set(d.items()), {('a', 'b'), ('c', 'd')})
+        d = PermaDict({"a": "b", "c": "d"})
+        self.assertEqual(set(d.keys()), {"a", "c"})
+        self.assertEqual(set(d.values()), {"b", "d"})
+        self.assertEqual(set(d.items()), {("a", "b"), ("c", "d")})
 
     def test_can_pop_key(self):
         d = PermaDict()
@@ -42,12 +42,12 @@ class PermaDictTests(unittest.TestCase):
 
     def test_can_update_with_new_keys(self):
         d = PermaDict()
-        d.update({'a': 1})
-        self.assertEqual(d, {'a': 1})
-        d.update([('b', 2)])
-        self.assertEqual(d, {'a': 1, 'b': 2})
+        d.update({"a": 1})
+        self.assertEqual(d, {"a": 1})
+        d.update([("b", 2)])
+        self.assertEqual(d, {"a": 1, "b": 2})
         d.update(c=3)
-        self.assertEqual(d, {'a': 1, 'b': 2, 'c': 3})
+        self.assertEqual(d, {"a": 1, "b": 2, "c": 3})
 
     def test_error_when_changing_value(self):
         d = PermaDict()
@@ -63,7 +63,8 @@ class PermaDictTests(unittest.TestCase):
         self.assertEqual(d, {1: 2, 3: 4, 5: 6})
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    unittest.expectedFailure
+
     def test_force_set_method(self):
         d = PermaDict({1: 2, 3: 4})
         d.force_set(3, 6)
@@ -71,7 +72,8 @@ class PermaDictTests(unittest.TestCase):
         self.assertEqual(d, {1: 2, 3: 6, 5: 6})
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    unittest.expectedFailure
+
     def test_silent_flag_to_initializer(self):
         d = PermaDict({1: 2, 3: 4}, silent=True)
         d.update([(5, 6), (1, 8), (7, 8)])
@@ -80,17 +82,18 @@ class PermaDictTests(unittest.TestCase):
         d[9] = 10
         self.assertEqual(d, {1: 2, 3: 4, 5: 6, 7: 8, 9: 10})
         e = PermaDict(silent=True, not_silent=False, super_silent=True)
-        self.assertEqual(e, {'not_silent': False, 'super_silent': True})
+        self.assertEqual(e, {"not_silent": False, "super_silent": True})
 
     # To test the Bonus part of this exercise, comment out the following line
-    @unittest.expectedFailure
+    unittest.expectedFailure
+
     def test_force_argument_to_update(self):
         d = PermaDict({1: 2, 3: 4}, silent=True)
         d.update([(5, 6), (1, 8), (7, 8)], force=True)
         self.assertEqual(d, {1: 8, 3: 4, 5: 6, 7: 8})
         e = PermaDict()
         e.update(a=1, b=2, force=True)
-        self.assertEqual(e, {'a': 1, 'b': 2})
+        self.assertEqual(e, {"a": 1, "b": 2})
 
 
 if __name__ == "__main__":
