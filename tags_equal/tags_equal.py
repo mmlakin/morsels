b3de2660c2a77ae3c26d492e467f135e2e90f7a6
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-06-08 18:35:51 -0700

    Add tags_equal initial solve all bonus

--- tags_equal/tags_equal.py
+++ tags_equal/tags_equal.py
@@ -9,14 +9,42 @@
 
 
 def clean_tag(tag: str) -> list:
-    tag = tag.replace("<", "")
-    tag = tag.replace(">", "")
-    tag = tag.replace("'", "")
-    tag = tag.replace('"', "")
+    """ Cleans a tag and returns a dict of tag keys and values, respecting the
+        first value set for a key """
+
+    # Remove markup symbols
+    markup = "<", ">"
+    for symbol in markup:
+        tag = tag.replace(symbol, "")
 
     tag = tag.upper()
     tag = tag.split(" ")
-    return set(tag)
+
+    tagdict = dict()
+    quotedstringkey = None
+    quotes = "'", '"'
+    for item in tag:
+        if quotedstringkey is not None:
+            previouskey = quotedstringkey
+            if item[-1] in quotes:
+                quotedstringkey = None
+            for quote in quotes:
+                item = item.replace(quote, "")
+            tagdict[previouskey] += f" {item}"
+            continue
+
+        item = item.split("=")
+        key = item[0]
+        if key not in tagdict.keys():
+            value = item[1] if len(item) > 1 else ""
+            if value != "":
+                if value[0] in quotes and value[-1] not in quotes:
+                    quotedstringkey = key
+                for quote in quotes:
+                    value = value.replace(quote, "")
+            tagdict[key] = value
+
+    return tagdict
 
 
 def tags_equal(tag1: str, tag2: str) -> bool:
