e7171323e00c3acb865ec0c60ceadb9c53402da4
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-06-08 19:08:06 -0700

    Update tags_equal using shlex

--- tags_equal/tags_equal.py
+++ tags_equal/tags_equal.py
@@ -8,45 +8,30 @@
 """
 
 
-def clean_tag(tag: str) -> list:
-    """ Returns a tuple containing the tag name and a dict of the tag's keys
-        and values, respecting the first value set for a key """
+import shlex
+
+
+def clean_tag(tag: str) -> tuple:
+    """Return a tuple containing the tag name and a dict of the tag's
+        attributes, respecting the first value set for an attribute"""
     tagdict = dict()
-    quotedstringkey = None
-    quotes = "'", '"'
 
     # Remove brackets around tag
     tag = tag[1:-1]
 
     tag = tag.upper()
 
-    name, *attributes = tag.split()
+    name, *attributes = shlex.split(tag)
 
     for item in attributes:
-        if quotedstringkey is not None:
-            previouskey = quotedstringkey
-            # If end of quoted string
-            if item[-1] in quotes:
-                quotedstringkey = None
-            for quote in quotes:
-                item = item.replace(quote, "")
-            tagdict[previouskey] += f" {item}"
-            continue
-
         item = item.split("=")
         key = item[0]
-        if key not in tagdict.keys():
-            value = item[1] if len(item) > 1 else ""
-            if value != "":
-                # If beginning of quoted string
-                if value[0] in quotes and value[-1] not in quotes:
-                    quotedstringkey = key
-                for quote in quotes:
-                    value = value.replace(quote, "")
-            tagdict[key] = value
+        value = item[1] if len(item) > 1 else ""
+        tagdict.setdefault(key, value)
 
     return name, tagdict
 
 
 def tags_equal(tag1: str, tag2: str) -> bool:
+    """Return true if the tags contain equal values"""
     return clean_tag(tag1) == clean_tag(tag2)
